68ebb6b3382c000dabc02eabc46d538b739931c1
mdparkes <pr3@KM3H2THGK9.local>
2024-02-21 14:33:51 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='keggpathwaygraphs',
+    version='',
+    packages=['keggpathwaygraphs'],
+    url='https://github.com/mdparkes/keggpathwaygraphs',
+    license='MIT',
+    author='Michael Parkes',
+    author_email='',
+    description='Python scripts for scraping KEGG BRITE orthology KGML data and constructing graph objects from it'
+)
